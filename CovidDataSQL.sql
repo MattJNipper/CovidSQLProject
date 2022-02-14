75a85cc85cf981a78d4ddef4dd9089d10b5673a7
MattJNipper <98855457+MattJNipper@users.noreply.github.com>
2022-02-13 23:20:08 -0500

    Added more queries.
    Worked with the data in the vaccines table as well as CTEs, Temp Tables and Views

--- CovidDataSQL.sql
+++ CovidDataSQL.sql
@@ -55,3 +55,98 @@ From CovidDataProject..CovidDeaths
 where continent is null
 Group by location
 order by TotalDeathCount desc
+
+-- Global info --
+
+-- Covid Mortality rate  time
+
+Select date, SUM(new_cases), SUM(cast(new_deaths as int)), SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+From CovidDataProject..CovidDeaths
+where continent is not null
+Group by date
+order by 1,2
+
+-- Covid Mortality rate  time
+
+Select SUM(new_cases), SUM(cast(new_deaths as int)), SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+From CovidDataProject..CovidDeaths
+where continent is not null
+order by 1,2
+
+-- Vaccine info --
+
+Select *
+From CovidDataProject..CovidDeaths dea
+Join CovidDataProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+
+-- Total pop vs total vaccines over time
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+From CovidDataProject..CovidDeaths dea
+Join CovidDataProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+order by 1,2,3
+
+
+-- Totaling vaccines over time for a rolling total # vaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location, dea.date) as RollingTotalVaxed
+From CovidDataProject..CovidDeaths dea
+Join CovidDataProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+order by 2,3
+
+-- CTEs --
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as(
+-- Totaling vaccines over time for a rolling total # vaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location, dea.date) as RollingTotalVaxed
+From CovidDataProject..CovidDeaths dea
+Join CovidDataProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+)
+
+Select *, (RollingPeopleVaccinated/Population)*100 as RollingVaxVSPopulation
+From PopvsVac
+
+-- Same thing but with Temp Table --
+
+DROP TABLE IF EXISTS CovidDataProject..#PercentPopVaccinated
+Create Table #PercentPopVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+Insert into #PercentPopVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location, dea.date) as RollingTotalVaxed
+From CovidDataProject..CovidDeaths dea
+Join CovidDataProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+
+Select *, (RollingPeopleVaccinated/Population)*100 as RollingVaxVSPopulation
+From #PercentPopVaccinated
+
+-- Creating views for use in Tableau --
+
+Create view PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location, dea.date) as RollingTotalVaxed
+From CovidDataProject..CovidDeaths dea
+Join CovidDataProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
